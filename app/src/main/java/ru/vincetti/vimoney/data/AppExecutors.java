33fea86060fc4b06670e31dead52e408bb0f69db
ivincetti <ivincetti@gmail.com>
2019-08-16 10:47:15 +0700

    Executor class

---
+++ app/src/main/java/ru/vincetti/vimoney/data/AppExecutors.java
@@ -0,0 +1,55 @@
+package ru.vincetti.vimoney.data;
+
+import android.os.Handler;
+import android.os.Looper;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+public class AppExecutors {
+    private final static Object LOCK = new Object();
+
+    private static AppExecutors sInstance;
+    private final Executor diskIO;
+    private final Executor networkIO;
+    private final Executor mainThreadIO;
+
+    public AppExecutors(Executor diskIO, Executor networkIO, Executor mainThreadIO) {
+        this.diskIO = diskIO;
+        this.networkIO = networkIO;
+        this.mainThreadIO = mainThreadIO;
+    }
+
+    public AppExecutors getsInstance() {
+        if (sInstance == null) {
+            synchronized (LOCK) {
+                sInstance = new AppExecutors(
+                        Executors.newSingleThreadExecutor(),
+                        Executors.newFixedThreadPool(2),
+                        new MainThreadExecutor());
+            }
+        }
+        return sInstance;
+    }
+
+    public Executor diskIO() {
+        return diskIO;
+    }
+
+    public Executor networkIO() {
+        return networkIO;
+    }
+
+    public Executor mainThreadIO() {
+        return mainThreadIO;
+    }
+
+    private class MainThreadExecutor implements Executor {
+        private Handler mainThreadHandler = new Handler(Looper.getMainLooper());
+
+        @Override
+        public void execute(Runnable runnable) {
+            mainThreadHandler.post(runnable);
+        }
+    }
+}
