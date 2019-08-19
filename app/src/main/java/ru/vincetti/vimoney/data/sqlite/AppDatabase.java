284d317e30dfca4f4554543a13c69aad9d373195
ivincetti <ivincetti@gmail.com>
2019-08-19 11:53:23 +0700

    migration

--- app/src/main/java/ru/vincetti/vimoney/data/sqlite/AppDatabase.java
+++ app/src/main/java/ru/vincetti/vimoney/data/sqlite/AppDatabase.java
@@ -43,7 +43,7 @@ public abstract class AppDatabase extends RoomDatabase {
         @Override
         public void migrate(final SupportSQLiteDatabase db) {
             db.execSQL("BEGIN TRANSACTION");
-            db.execSQL("CREATE TABLE acc_backup(id integer PRIMARY KEY, name text, type text, sum integer)");
+            db.execSQL("CREATE TABLE acc_backup(id INTEGER PRIMARY KEY NOT NULL, name TEXT, type TEXT, sum DECIMAL NOT NULL)");
             db.execSQL("INSERT INTO acc_backup (id, name, type, sum) SELECT id, name, type, sum FROM accounts");
             db.execSQL("DROP TABLE accounts");
             db.execSQL("COMMIT");
