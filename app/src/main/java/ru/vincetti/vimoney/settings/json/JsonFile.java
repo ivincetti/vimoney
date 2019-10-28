e19ed18db103f483e4a4cb95dec005421fcf99a9
ivincetti <ivincetti@gmail.com>
2019-10-28 14:57:08 +0700

    read from External Done (Android / data / package)

--- app/src/main/java/ru/vincetti/vimoney/settings/json/JsonFile.java
+++ app/src/main/java/ru/vincetti/vimoney/settings/json/JsonFile.java
@@ -2,12 +2,14 @@ package ru.vincetti.vimoney.settings.json;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Environment;
 import android.text.TextUtils;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -37,71 +39,70 @@ public class JsonFile {
         final AppDatabase db = AppDatabase.getInstance(context);
 
         AppExecutors.getsInstance().diskIO().execute(() -> {
-            FileInputStream fisTransactions = null;
-            FileInputStream fisAccounts = null;
-            final StringBuilder transactionsJsonBuilder = new StringBuilder();
-            final StringBuilder accountsJsonBuilder = new StringBuilder();
+            String transactionsJson;
+            String accountsJson;
 
-            try {
-                fisTransactions = context.openFileInput(FILE_NAME_TRANSACTIONS);
-                InputStreamReader isr = new InputStreamReader(fisTransactions);
-                BufferedReader br = new BufferedReader(isr);
-                String text;
+            if (isExternalMounted()){
+                transactionsJson = readExternalFile(context, FILE_NAME_TRANSACTIONS);
+                accountsJson = readExternalFile(context, FILE_NAME_ACCOUNTS);
 
-                while ((text = br.readLine()) != null) {
-                    transactionsJsonBuilder.append(text).append("\n");
+                if (!TextUtils.isEmpty(transactionsJson) && !TextUtils.isEmpty(accountsJson)) {
+                    db.transactionDao()
+                            .deleteAllTransactions()
+                            .doOnComplete(() -> {
+                                Type listType = new TypeToken<ArrayList<TransactionModel>>() {}.getType();
+                                List<TransactionModel> transactions = gson.fromJson(transactionsJson, listType);
+                                for (TransactionModel transaction : transactions) {
+                                    db.transactionDao().insertTransaction(transaction);
+                                }
+                                db.accountDao()
+                                        .deleteAllAccounts()
+                                        .doOnComplete(() -> {
+                                            Type listType1 = new TypeToken<ArrayList<AccountModel>>() {}.getType();
+                                            List<AccountModel> transactions1 = gson.fromJson(accountsJson, listType1);
+                                            for (AccountModel account : transactions1) {
+                                                db.accountDao().insertAccount(account);
+                                            }
+                                        })
+                                        .subscribe();
+                            })
+                            .subscribe();
                 }
-
-                fisAccounts = context.openFileInput(FILE_NAME_ACCOUNTS);
-                isr = new InputStreamReader(fisAccounts);
-                br = new BufferedReader(isr);
-
-                while ((text = br.readLine()) != null) {
-                    accountsJsonBuilder.append(text).append("\n");
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (fisTransactions != null) {
-                    try {
-                        fisTransactions.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                if (fisAccounts != null) {
-                    try {
-                        fisAccounts.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            if (!TextUtils.isEmpty(transactionsJsonBuilder.toString())) {
-                db.transactionDao()
-                        .deleteAllTransactions()
-                        .doOnComplete(() -> {
-                            Type listType = new TypeToken<ArrayList<TransactionModel>>() {}.getType();
-                            List<TransactionModel> transactions = gson.fromJson(transactionsJsonBuilder.toString(), listType);
-                            for (TransactionModel transaction : transactions) {
-                                db.transactionDao().insertTransaction(transaction);
-                            }
-                            db.accountDao()
-                                    .deleteAllAccounts()
-                                    .doOnComplete(() -> {
-                                        Type listType1 = new TypeToken<ArrayList<AccountModel>>() {}.getType();
-                                        List<AccountModel> transactions1 = gson.fromJson(accountsJsonBuilder.toString(), listType1);
-                                        for (AccountModel account : transactions1) {
-                                            db.accountDao().insertAccount(account);
-                                        }
-                                    })
-                                    .subscribe();
-                        })
-                        .subscribe();
             }
         });
+    }
+
+    private static String readExternalFile(Context context, String fileName) {
+        FileInputStream fis = null;
+        final StringBuilder JsonBuilder = new StringBuilder();
+        try {
+            File myExternalFile = new File(context.getExternalFilesDir(null), fileName);
+            fis = new FileInputStream(myExternalFile);
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+            String text;
+
+            while ((text = br.readLine()) != null) {
+                JsonBuilder.append(text).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        return JsonBuilder.toString();
+    }
+
+    private static boolean isExternalMounted() {
+        //Checking the availability state of the External Storage.
+        return Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState());
     }
 
 }
