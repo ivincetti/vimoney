993fbbdfb85492a4d1c0fa9916aa13ba62c67186
ivincetti <ivincetti@gmail.com>
2022-07-29 13:17:52 +0700

    viewmodel with actions

---
+++ app/src/main/java/ru/vincetti/vimoney/models/SplashModel.kt
@@ -0,0 +1,106 @@
+package ru.vincetti.vimoney.models
+
+import ru.vincetti.modules.core.models.*
+import ru.vincetti.modules.core.models.Currency
+import ru.vincetti.modules.database.repository.*
+import ru.vincetti.modules.network.ConfigLoader
+import ru.vincetti.modules.network.models.*
+import ru.vincetti.vimoney.utils.SampleDescription
+import java.util.*
+import javax.inject.Inject
+
+class SplashModel @Inject constructor(
+    private val accountRepo: AccountRepo,
+    private val transactionRepo: TransactionRepo,
+    private val configRepo: ConfigRepo,
+    private val categoryRepo: CategoryRepo,
+    private val currencyRepo: CurrencyRepo,
+    private val configLoader: ConfigLoader,
+    private val sampleDescription: SampleDescription
+) {
+
+    suspend fun loadConfigByKey(): Config? = configRepo.loadByKey()
+
+    suspend fun updateConfig() {
+        val configModel = configLoader.loadPreferences()
+        configDbUpdate(configModel)
+    }
+
+    private suspend fun configDbUpdate(response: ConfigFile) {
+        configDbDateInsert(response.dateEdit)
+        currencyImport(response.currency)
+        accountsUpdate(response.accounts)
+        categoriesUpdate(response.categories)
+        transactionsImport(response.transactions)
+    }
+
+    private suspend fun configDbDateInsert(timeMillisLong: Long) {
+        configRepo.add(timeMillisLong.toString())
+    }
+
+    private suspend fun transactionsImport(transactionItems: List<TransactionsItem>) {
+        val transactions = mutableListOf<Transaction>()
+        transactionItems.map {
+            transactions.add(
+                Transaction(
+                    Date(it.date),
+                    it.accountId,
+                    sampleDescription.get(),
+                    it.type,
+                    it.sum.toFloat()
+                )
+            )
+        }
+        transactionRepo.add(transactions)
+        updateAccounts(accountRepo, transactionRepo)
+    }
+
+    private suspend fun currencyImport(currencyItems: List<CurrencyItem>) {
+        currencyItems.forEach {
+            currencyRepo.add(
+                Currency(it.code, it.name, it.symbol)
+            )
+        }
+    }
+
+    private suspend fun accountsUpdate(accountsItems: List<AccountsItem>) {
+        for (acc in accountsItems) {
+            accountUpdate(
+                acc.id,
+                acc.type,
+                acc.title,
+                acc.balance
+            )
+        }
+    }
+
+    private suspend fun accountUpdate(accId: Int, type: String, title: String, balance: Int) {
+        val newAcc = Account(accId, title, type, balance)
+        accountRepo.add(newAcc)
+    }
+
+    private suspend fun categoriesUpdate(categoriesItems: List<CategoriesItem>) {
+        categoriesItems.forEach {
+            categoryUpdate(
+                it.categoryId,
+                it.name,
+                it.symbol
+            )
+        }
+    }
+
+    private suspend fun categoryUpdate(catId: Int, name: String, symbol: String) {
+        val newCat = Category(catId, name, symbol)
+        categoryRepo.add(newCat)
+    }
+
+    private suspend fun updateAccounts(
+        accountRepo: AccountRepo,
+        transactionRepo: TransactionRepo
+    ) {
+        accountRepo.loadAll().forEach {
+            val sum = transactionRepo.loadBalanceByCheckId(it.id)
+            accountRepo.updateAccountBalanceById(it.id, sum)
+        }
+    }
+}
--- app/src/main/java/ru/vincetti/vimoney/ui/splash/SplashViewModel.kt
+++ app/src/main/java/ru/vincetti/vimoney/ui/splash/SplashViewModel.kt
@@ -1,40 +1,25 @@
 package ru.vincetti.vimoney.ui.splash
 
 import android.util.Log
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
-import ru.vincetti.modules.core.models.Account
-import ru.vincetti.modules.core.models.Category
-import ru.vincetti.modules.core.models.Currency
-import ru.vincetti.modules.core.models.Transaction
 import ru.vincetti.modules.core.utils.SingleLiveEvent
-import ru.vincetti.modules.database.repository.*
-import ru.vincetti.modules.network.ConfigLoader
-import ru.vincetti.modules.network.models.*
+import ru.vincetti.vimoney.models.SplashModel
 import ru.vincetti.vimoney.utils.NetworkUtils
-import ru.vincetti.vimoney.utils.SampleDescription
-import java.util.*
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val transactionRepo: TransactionRepo,
-    private val accountRepo: AccountRepo,
-    private val configRepo: ConfigRepo,
-    private val categoryRepo: CategoryRepo,
-    private val currencyRepo: CurrencyRepo,
-    private val configLoader: ConfigLoader,
-    private val networkUtils: NetworkUtils,
-    private val sampleDescription: SampleDescription
+    private val splashModel: SplashModel,
+    private val networkUtils: NetworkUtils
 ) : ViewModel() {
 
-    val networkError = SingleLiveEvent<Boolean>()
-
-    val need2Navigate2Home = SingleLiveEvent<Boolean>()
-
-    val need2Navigate2Self = SingleLiveEvent<Boolean>()
+    private val _action = SingleLiveEvent<Action>()
+    val action: LiveData<Action>
+        get() = _action
 
     init {
         checkDb()
@@ -42,100 +27,41 @@ class SplashViewModel @Inject constructor(
 
     private fun checkDb() {
         viewModelScope.launch {
-            val config = configRepo.loadByKey()
-            if (config == null) {
-                if (networkUtils.isAvailable()) {
-                    loadJsonFromServer()
-                } else {
-                    networkError.value = true
-                }
+            val isConfigExist = splashModel.loadConfigByKey() != null
+
+            if (isConfigExist) {
+                _action.value = Action.NavigateMain
             } else {
-                need2Navigate2Home.value = true
+                startLoadingConfig()
             }
         }
     }
 
     fun resetNetworkStatus() {
-        need2Navigate2Self.value = true
+        _action.value = Action.NavigateSelf
     }
 
-    private fun loadJsonFromServer() {
-        viewModelScope.launch {
-            try {
-                val configModel = configLoader.loadPreferences()
-                configDbUpdate(configModel)
-                need2Navigate2Home.value = true
-            } catch (e: Exception) {
-                Log.d("TAG", " load from json error ${e.message}")
-                networkError.value = true
-            }
+    private suspend fun startLoadingConfig() {
+        if (networkUtils.isAvailable()) {
+            loadJsonFromServer()
+        } else {
+            _action.value = Action.Error
         }
     }
 
-    private suspend fun configDbUpdate(response: ConfigFile) {
-        configDbDateInsert(response.dateEdit)
-        currencyImport(response.currency)
-        accountsUpdate(response.accounts)
-        categoriesUpdate(response.categories)
-        transactionsImport(response.transactions)
-    }
-
-    private suspend fun configDbDateInsert(timeMillisLong: Long) {
-        configRepo.add(timeMillisLong.toString())
-    }
-
-    private suspend fun transactionsImport(transactionItems: List<TransactionsItem>) {
-        val transactions = mutableListOf<Transaction>()
-        transactionItems.map {
-            transactions.add(
-                Transaction(
-                    Date(it.date),
-                    it.accountId,
-                    sampleDescription.get(),
-                    it.type,
-                    it.sum.toFloat()
-                )
-            )
-        }
-        transactionRepo.add(transactions)
-    }
-
-    private suspend fun currencyImport(currencyItems: List<CurrencyItem>) {
-        currencyItems.forEach {
-            currencyRepo.add(
-                Currency(it.code, it.name, it.symbol)
-            )
+    private suspend fun loadJsonFromServer() {
+        try {
+            splashModel.updateConfig()
+            _action.value = Action.NavigateMain
+        } catch (e: Exception) {
+            Log.d("TAG", " load from json error ${e.message}")
+            _action.value = Action.Error
         }
     }
 
-    private suspend fun accountsUpdate(accountsItems: List<AccountsItem>) {
-        for (acc in accountsItems) {
-            accountUpdate(
-                acc.id,
-                acc.type,
-                acc.title,
-                acc.balance
-            )
-        }
-    }
-
-    private suspend fun accountUpdate(accId: Int, type: String, title: String, balance: Int) {
-        val newAcc = Account(accId, title, type, balance, 810)
-        accountRepo.add(newAcc)
-    }
-
-    private suspend fun categoriesUpdate(categoriesItems: List<CategoriesItem>) {
-        categoriesItems.forEach {
-            categoryUpdate(
-                it.categoryId,
-                it.name,
-                it.symbol
-            )
-        }
-    }
-
-    private suspend fun categoryUpdate(catId: Int, name: String, symbol: String) {
-        val newCat = Category(catId, name, symbol)
-        categoryRepo.add(newCat)
+    sealed class Action {
+        object NavigateSelf : Action()
+        object NavigateMain : Action()
+        object Error : Action()
     }
 }
